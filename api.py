28058ce23e2ac8784466dbe3d5370622939b0e7a
Your 1rHub <ibadmuhammadirsyadul@gmail.com>
2025-06-01 15:15:07 +0700

    Menambah Folder API

---
+++ api.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask import request
+from pymongo import MongoClient
+import bcrypt
+
+app = Flask(__name__)
+CORS(app)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['uhome']
+collectionUser = db['user']
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+
+    resultuser = collectionUser.find_one({'email': email})
+
+    if resultuser:
+        if bcrypt.checkpw(password.encode(), resultuser['password'].encode()):
+            return {'status': 'success'}
+        else:
+            return {'status': 'error', 'msg': 'email atau password salah'}
+    else:
+        return {'status': 'error', 'msg': 'email atau password salah'}
+
+    
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    # cek apakah email sudah terdaftar
+    existing_user = collectionUser.find_one({'email': data['email']})
+    
+    if existing_user:
+        return {
+            'status': 'error',
+            'msg': 'Email sudah terdaftar'
+        }
+
+    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    new_user = {
+        'nama': data['nama'],
+        'email': data['email'],
+        'password': hashed_password.decode('utf-8'),
+        'telepon': data['telepon'],
+    }
+    collectionUser.insert_one(new_user)
+
+    return {
+        'status': 'success',
+        'msg': 'Registrasi berhasil'
+    }
